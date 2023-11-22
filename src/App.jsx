cd471e833ccd537c89641c41b47958d6e4ac130c
JFmaia <jfmaia.dev@gmail.com>
2023-11-22 20:41:10 -0300

    Init create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Box, Text, Button} from '@chakra-ui/react'
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Box pt={4} pb={4} pr={120} pl={120} shadow={'base'} display={'flex'} alignItems={'center'} justifyContent={'space-between'} bg={"#060118"}>
+      <Text color='#fff' fontSize={'1.4rem'} fontFamily={'fantasy'}>Logo</Text>
+      <Box display={'flex'} gap={10} alignItems={'center'}>
+        <Button p={1} rounded={4} color={'#fff'} bg={"none"} border={1}> Page one </Button>
+        <Button p={1} rounded={4} color={'#fff'} bg={"none"} border={1}> Page two </Button>
+        <Button p={1} rounded={4} color={'#fff'} bg={"none"} border={1}> Page tree </Button>
+        <Button p={1} rounded={4}  color={'#fff'} bg={'none'} border={1}> Page four </Button>
+      </Box>
+    </Box>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
